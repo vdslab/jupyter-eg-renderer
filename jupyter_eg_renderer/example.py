d4ce947269dcd43cb19d9e4c39eaf6f5a6ef28c6
moriakijp <moriakijp@gmail.com>
2019-07-18 17:17:30 +0900

    Imprement default_node_fill_color

--- jupyter_eg_renderer/example.py
+++ jupyter_eg_renderer/example.py
@@ -13,9 +13,21 @@ class EgRenderer(widgets.DOMWidget):
     _model_module = Unicode('jupyter-eg-renderer').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
-    # layout_method = Unicode('hierarchy').tag(sync=True)
     data = Unicode('{"nodes": [], "links": []}').tag(sync=True)
+    # layout_method = Unicode('hierarchy').tag(sync=True)
+
+    # Data Loading
+ 
+    # Node Geometry
+    default_node_width=Unicode('10').tag(sync=True)
+    default_node_height=Unicode('10').tag(sync=True)
+
+    # Node Representation
     default_node_fill_color = Unicode('white').tag(sync=True)
+    default_node_fill_opacity = Unicode('1').tag(sync=True)
+    default_node_stroke_color = Unicode('white').tag(sync=True)
+    default_node_label = Unicode('').tag(sync=True)
+    
 
     def render(self, graph):
         data = json_graph.node_link_data(graph)
